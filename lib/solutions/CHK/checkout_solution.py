1f0099e3948a5cde30c73f7840a4eb26fc7651cb
sbx_user1051 <sbx_user1051@169.254.33.57>
2022-03-22 19:38:06 +0000

    Tue Mar 22 19:38:06 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,7 @@
 # skus = unicode string
 def checkout(skus):
 
-    # Assume that shopping list will arrive into function as a series of skus followed by the item letter #
+    # Assume that lower case are invalid #
     
     totalSum = 0
     
@@ -25,22 +25,24 @@ def checkout(skus):
                 c_sum += 1
             elif c == "D":
                 d_sum += 1
+        else:
+            return -1
                 
-        elif c.isalpha():
-            if c == "A":
-                # carry out the special offer as many times as possible then the regular price for those items left over
-                totalSum += (n // 3) * 130
-                totalSum += (n % 3) * 50
-            elif c == "B":
-                totalSum += (n // 2) * 45
-                totalSum += (n % 2) * 30
-            elif c == "C":
-                totalSum += n * 20
-            elif c == "D":
-                totalSum += n * 15
-            # reset the value to calculate the next sku for the next item
-            n = 0
+    # carry out the special offer as many times as possible then the regular price for those items left over
+
+    #~A~#
+    totalSum += (a_sum // 3) * 130
+    totalSum += (a_sum % 3) * 50
+    #~B~#
+    totalSum += (b_sum // 2) * 45
+    totalSum += (b_sum % 2) * 30
+    #~C~#
+    totalSum += c_sum * 20
+    #~D~#
+    totalSum += d_sum * 15
+
         
     return totalSum
+
 
 
