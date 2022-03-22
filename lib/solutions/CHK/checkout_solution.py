de82b4937266757308413ab014b5cff3d6bbb810
sbx_user1051 <sbx_user1051@169.254.33.57>
2022-03-22 19:31:17 +0000

    Tue Mar 22 19:31:17 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,34 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    # Assume that shopping list will arrive into function as a series of skus followed by the item letter #
+    
+    totalSum = 0
+
+    n = 0
+    
+    for c in skus:
+        
+        if c.isdigit():
+            n *= 10
+            n += int(c)
+            # if there are two isdigits in a row we are dealing with a 2 digit number and the calculation is made accordingly
+            
+        elif c.isalpha():
+            if c == "A":
+                # carry out the special offer as many times as possible then the regular price for those items left over
+                totalSum += (n // 3) * 130
+                totalSum += (n % 3) * 50
+            elif c == "B":
+                totalSum += (n // 2) * 45
+                totalSum += (n % 2) * 30
+            elif c == "C":
+                totalSum += n * 20
+            elif c == "D":
+                totalSum += n * 15
+            # reset the value to calculate the next sku for the next item
+            n = 0
+        
+    return totalSum
+
